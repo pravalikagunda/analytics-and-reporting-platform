1d47800058c4da68d80bd0b299bab418f007373e
PRAVALIKA GUNDA <pravalikagunda1456@gmail.com>
2026-05-26 12:34:25 +0530

    Update utils.py

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta, timezone
 from pathlib import Path
 from typing import Any
 
-import emails  # type: ignore[import-untyped]
 import jwt
 from jinja2 import Template
 from jwt.exceptions import InvalidTokenError
@@ -28,32 +27,6 @@ def render_email_template(*, template_name: str, context: dict[str, Any]) -> str
     ).read_text()
     html_content = Template(template_str).render(context)
     return html_content
-
-
-def send_email(
-    *,
-    email_to: str,
-    subject: str = "",
-    html_content: str = "",
-) -> None:
-    assert settings.emails_enabled, "no provided configuration for email variables"
-    message = emails.Message(
-        subject=subject,
-        html=html_content,
-        mail_from=(settings.EMAILS_FROM_NAME, settings.EMAILS_FROM_EMAIL),
-    )
-    smtp_options = {"host": settings.SMTP_HOST, "port": settings.SMTP_PORT}
-    if settings.SMTP_TLS:
-        smtp_options["tls"] = True
-    elif settings.SMTP_SSL:
-        smtp_options["ssl"] = True
-    if settings.SMTP_USER:
-        smtp_options["user"] = settings.SMTP_USER
-    if settings.SMTP_PASSWORD:
-        smtp_options["password"] = settings.SMTP_PASSWORD
-    response = message.send(to=email_to, smtp=smtp_options)
-    logger.info(f"send email result: {response}")
-
 
 def generate_test_email(email_to: str) -> EmailData:
     project_name = settings.PROJECT_NAME
